e6eab229ccf5a2cbadf9f9ec3078c8b185a3d4fe
Anna Valéria Makari <94723781+OnnaMcadva@users.noreply.github.com>
2024-04-23 20:34:02 +0200

    Update ft_putendl_fd.c

--- libft/ft_putendl_fd.c
+++ libft/ft_putendl_fd.c
@@ -1,11 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putendl_fd_v.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: anmakaro <anmakaro@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/05 12:08:27 by anmakaro          #+#    #+#             */
+/*   Updated: 2023/12/05 13:16:51 by anmakaro         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "libft.h"
 
-void	ft_putendl_fd(const char *s, int fd)
+void	ft_putendl_fd(char *s, int fd)
 {
 	size_t	i;
 
 	if (!s)
+	{
 		return ;
+	}
 	i = 0;
 	while (s[i])
 	{
